5fecaa6f862303050f5c1a900f0cafb0f26382f3
shidash <shidash@shidash.com>
2015-11-03 17:41:53 -0500

    Uses new requestmanager

--- lib/linkedincrawler.rb
+++ lib/linkedincrawler.rb
@@ -1,45 +1,61 @@
+require 'requestmanager'
 require 'linkedinparser'
 require 'generalscraper'
+
 require 'selenium-webdriver'
 require 'pry'
 
 class LinkedinCrawler
-  include ProxyManager
-  def initialize(search_terms)
+  def initialize(search_terms, retry_limit, proxy_list, request_time)
     @search_terms = search_terms
     @output = Array.new
+    @retry_limit = retry_limit
+    @retry_count = 0
+    @proxy_list = proxy_list
+    @requests = RequestManager.new(@proxy_list, request_time, 5)
   end
 
   # Run search terms and get results
   def search
     # Run Google search
-    g = GeneralScraper.new("site:linkedin.com/pub", @search_terms,  "/home/shidash/proxies", false)
+    g = GeneralScraper.new("site:linkedin.com/pub -site:linkedin.com/pub/dir/", @search_terms, @proxy_list)
     
     # Scrape each resulting LinkedIn page
-    gen_driver
     JSON.parse(g.getURLs).each do |profile|
-      scrape(profile)
+      if profile.include?(".linkedin.") && !profile.include?("/search")
+        scrape(profile)
+      end
     end
+
+    # Close all the browsers
+    @requests.close_all_browsers
   end
 
-  # Generate driver for searches
-  def gen_driver
-    profile = Selenium::WebDriver::Firefox::Profile.new
-    profile['intl.accept_languages'] = 'en'
-    profile["javascript.enabled"] = false
-    @driver = Selenium::WebDriver.for :firefox, profile: profile
+  # Check that it is actually a LinkedIn profile page
+  def check_right_page(profile_url)
+    return !profile_url.include?("www.google") &&
+           !profile_url.include?("linkedin.com/pub/dir") &&
+           !profile_url.include?("/search") &&
+           @retry_count < @retry_limit
   end
 
   # Scrape each page
   def scrape(profile_url)
     # Get profile page
-    profile_html = getPage(profile_url, @driver, nil, 5, false).page_source
+    profile_html = @requests.get_page(profile_url)
 
     # Parse profile and add to output
     begin
       l = LinkedinParser.new(profile_html, profile_url, {timestamp: Time.now})
       @output += JSON.parse(l.results_by_job)
+      @retry_count = 0
     rescue
+      # If proxy doesn't work, try another a few times
+      if check_right_page(profile_url)
+        @requests.restart_browser
+        @retry_count += 1
+        scrape(profile_url)
+      end
     end
   end
 
--- linkedincrawler.gemspec
+++ linkedincrawler.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'linkedincrawler'
-  s.version     = '0.0.4'
-  s.date        = '2015-11-01'
+  s.version     = '0.0.5'
+  s.date        = '2015-11-03'
   s.summary     = 'Crawls public LinkedIn profiles'
   s.description = 'Crawls public LinkedIn profiles via Google'
   s.authors     = ['M. C. McGrath']
